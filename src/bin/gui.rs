09a40872adef629c678b50f5661f7efd635cb4f4
David Weis <dweis7@gmail.com>
2022-01-27 05:18:17 +0000

    Working version!

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -3,41 +3,82 @@ use iced::Application;
 use reddit_browser::reddit_gallery_api;
 use roux::util::{FeedOption, TimePeriod};
 use roux::Subreddit;
+use std::collections::VecDeque;
 
 fn is_simple_reddit_image(url: &str) -> bool {
     url.starts_with("https://i.redd.it/")
 }
 
-#[derive(Debug)]
-enum ImagePostType {
-    Image(String),
-    Gallery(Vec<String>),
+pub fn main() -> iced::Result {
+    BunnyBrowser::run(iced::Settings::default())
 }
 
-struct RedditImageBrowser {
+async fn fetch_image(url: String) -> Result<iced::image::Handle> {
+    println!("Fetching {}", url);
+    let bytes = reqwest::get(&url).await?.bytes().await?;
+    Ok(iced::image::Handle::from_memory(bytes.as_ref().to_vec()))
+}
+
+struct ImageSearcher {
     subreddit: Subreddit,
-    feed_search_options: FeedOption,
-    current_feed_options: FeedOption,
+    next_feed_search_options: FeedOption,
+    images: VecDeque<String>,
 }
 
-impl RedditImageBrowser {
-    fn new(subreddit_name: &str) -> Result<Self> {
+impl Clone for ImageSearcher {
+    fn clone(&self) -> Self {
+        Self {
+            subreddit: Subreddit::new(&self.subreddit.name),
+            next_feed_search_options: self.next_feed_search_options.clone(),
+            images: self.images.clone(),
+        }
+    }
+}
+
+impl std::fmt::Debug for ImageSearcher {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ImageSearcher")
+            .field("subreddit", &self.subreddit.name)
+            .field("next_feed_search_options", &self.next_feed_search_options)
+            .field("images", &self.images)
+            .finish()
+    }
+}
+
+impl ImageSearcher {
+    fn new(subreddit_name: &str) -> Self {
         let subreddit = Subreddit::new(subreddit_name);
-        let feed_search_options = FeedOption::new().period(TimePeriod::ThisYear);
-        let current_feed_options = feed_search_options.clone();
-        Ok(Self {
+        let next_feed_search_options = FeedOption::new().period(TimePeriod::ThisYear);
+        Self {
             subreddit,
-            feed_search_options,
-            current_feed_options,
-        })
+            next_feed_search_options,
+            images: VecDeque::new(),
+        }
     }
 
-    async fn pull_image_posts(&mut self) -> Result<Vec<ImagePostType>> {
-        let mut posts = vec![];
+    fn with_next_search(subreddit: Subreddit, next_feed: FeedOption, images: Vec<String>) -> Self {
+        Self {
+            subreddit,
+            next_feed_search_options: next_feed,
+            images: images.into(),
+        }
+    }
+
+    fn get_image_link(&self) -> Option<String> {
+        self.images.get(0).cloned()
+    }
+
+    async fn search_next(mut self) -> Result<Self> {
+        // short circuit
+        if !self.images.is_empty() {
+            self.images.pop_front();
+            return Ok(self);
+        }
         let search_results = self
             .subreddit
-            .top(25, Some(self.current_feed_options.clone()))
+            .top(25, Some(self.next_feed_search_options.clone()))
             .await?;
+        let mut posts = vec![];
         for post in search_results.data.children {
             if let Some(url) = post.data.url {
                 // image post
@@ -45,38 +86,38 @@ impl RedditImageBrowser {
                     // Should this save the gallery link?
                     let post_links =
                         reddit_gallery_api::pull_image_links_from_gallery(&url).await?;
-                    posts.push(ImagePostType::Gallery(post_links));
+                    posts.extend(post_links);
                 }
                 if is_simple_reddit_image(&url) {
-                    posts.push(ImagePostType::Image(url.to_owned()));
+                    posts.push(url.to_owned());
                 }
             }
         }
+
         let after_search_token = search_results.data.after.unwrap();
-        self.current_feed_options = self.feed_search_options.clone().after(&after_search_token);
-        Ok(posts)
+        let next_feed_options = self
+            .next_feed_search_options
+            .clone()
+            .after(&after_search_token);
+
+        Ok(Self::with_next_search(
+            self.subreddit,
+            next_feed_options,
+            posts,
+        ))
     }
-}
-
-pub fn main() -> iced::Result {
-    BunnyBrowser::run(iced::Settings::default())
-}
-
-async fn fetch_image(url: &str) -> Result<iced::image::Handle> {
-    let bytes = reqwest::get(url).await?.bytes().await?;
-    Ok(iced::image::Handle::from_memory(bytes.as_ref().to_vec()))
 }
 
 #[derive(Debug)]
 enum Message {
-    Images(Result<(Vec<ImagePostType>, iced::image::Handle)>),
+    ImageSearchFinished(Result<ImageSearcher>),
+    ImageFetched(Result<iced::image::Handle>, ImageSearcher),
+    SleepElapsed(ImageSearcher),
 }
 
 enum AppState {
     Loading,
     Loaded {
-        #[allow(dead_code)]
-        images: Vec<ImagePostType>,
         image: iced::image::Handle,
         image_view_state: iced::image::viewer::State,
     },
@@ -94,16 +135,6 @@ impl BunnyBrowser {
     }
 }
 
-async fn search_images() -> Result<(Vec<ImagePostType>, iced::image::Handle)> {
-    let mut browser = RedditImageBrowser::new("Rabbits").unwrap();
-    let data = browser.pull_image_posts().await?;
-    let first_image_link = match &data[0] {
-        ImagePostType::Image(link) => link.clone(),
-        ImagePostType::Gallery(links) => links[0].clone(),
-    };
-    Ok((data, fetch_image(&first_image_link).await?))
-}
-
 impl iced::Application for BunnyBrowser {
     type Executor = iced::executor::Default;
     type Message = Message;
@@ -111,9 +142,10 @@ impl iced::Application for BunnyBrowser {
 
     fn new(_flags: ()) -> (Self, iced::Command<Self::Message>) {
         let app = BunnyBrowser::new();
+        let image_searcher = ImageSearcher::new("Rabbits");
         (
             app,
-            iced::Command::perform(search_images(), Message::Images),
+            iced::Command::perform(image_searcher.search_next(), Message::ImageSearchFinished),
         )
     }
 
@@ -127,22 +159,34 @@ impl iced::Application for BunnyBrowser {
         _clipboard: &mut iced::Clipboard,
     ) -> iced::Command<Self::Message> {
         match message {
-            Message::Images(images) => {
-                let images = images.unwrap();
+            Message::ImageSearchFinished(image_search_result) => {
+                let searcher = image_search_result.unwrap();
+                let image_link = searcher.get_image_link().unwrap();
+                // TODO(David): Why can't I move searcher here?
+                iced::Command::perform(fetch_image(image_link), move |res| {
+                    Message::ImageFetched(res, searcher.clone())
+                })
+            }
+            Message::ImageFetched(image_result, searcher) => {
+                let image = image_result.unwrap();
                 self.state = AppState::Loaded {
-                    images: images.0,
-                    image: images.1,
+                    image,
                     image_view_state: iced::image::viewer::State::new(),
                 };
+                iced::Command::perform(
+                    tokio::time::sleep(std::time::Duration::from_secs(2)),
+                    move |_| Message::SleepElapsed(searcher.clone()),
+                )
+            }
+            Message::SleepElapsed(searcher) => {
+                iced::Command::perform(searcher.search_next(), Message::ImageSearchFinished)
             }
         }
-        iced::Command::none()
     }
 
     fn view(&mut self) -> iced::Element<Self::Message> {
         match &mut self.state {
             AppState::Loaded {
-                images: _,
                 image,
                 image_view_state,
             } => {
